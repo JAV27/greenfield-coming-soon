1a74d48b7ec3319e256ebf76a3414b5eb1e7cfa4
Josh Viktorov <joshviktorov27@gmail.com>
2021-06-01 21:01:46 -0400

    search bar initial commit

--- showcase.js
+++ showcase.js
@@ -2,7 +2,7 @@ function addPlayerCard(el, index) {
 
     $('.main.showcase .row').append(`
     <div class="col-6 col-sm-4 col-lg-3">
-        <div class="card" style="text-align: center;">
+        <div class="card player" style="text-align: center;">
             <img src="images/` + el.img + `" class="card-img-top" style="width: 25%; margin: auto;">
             <div class="card-body">
                 <h5 class="card-title">` + el.name + `</h5>
@@ -14,6 +14,20 @@ function addPlayerCard(el, index) {
     </div>
     `)
 
+}
+
+function search_players() {
+
+    let search = $('.nav input').val().toLowerCase().replace(/\s+/g, '');
+
+    $('.card.player').each(function() {
+        let name = $(this).children('.card-body').children('.card-title').text().toLowerCase().replace(/\s+/g, '');
+
+        if(name == search) {
+            console.log(name)
+        }
+
+    });
 }
 
 $(document).ready(function() {
