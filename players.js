567570d31d644355eed1bb975e448cad112dc570
Josh Viktorov <joshviktorov27@gmail.com>
2021-06-08 22:09:20 -0400

    connected player data to player page

--- players.js
+++ players.js
@@ -1,10 +1,45 @@
+function addTeam(el, index) {
+    
+    let players = ``;
+
+    el.players.forEach(function(el, index) {
+        players += `
+        <div class="player">
+            <img src="images/` + el.img + `">
+            <div class="info">
+                <p class="name"> #` + el.num + ` ` + el.name + `</p>
+                <p>` + el.club + `</p>
+                <p>` + el.hs + ` ('` + el.class + `')</p>
+                <p>Positions: ` + el.positions + `</p>
+            </div>
+        </div>
+    `;
+    });
+    
+    $('.main.players .teams').append(`
+        <div class="team">
+            <div class="top">
+                <img src="images/` + el.img + `" alt="">
+                <p>` + el.name + `</p>
+            </div>
+            <hr>
+            <div class="body">
+            ` + players + `
+            </div>
+        </div>
+    `);
+
+}
+
 $(document).ready(function() {
 
-    $.getJSON("https://greenfieldleagues.com/players.json", function(data) {
+    $.getJSON("https://greenfieldleagues.com/data.json", function(data) {
         
-        let players = data.players;
+        let teams = data.teams;
 
-        console.log(players)
+        console.log(teams)
+
+        teams.forEach(addTeam); 
     
     });
 
