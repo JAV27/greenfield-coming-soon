dba1d5933ab3bc1080d4de5f597c0eb1cd1fea3f
Josh Viktorov <joshviktorov27@gmail.com>
2021-06-10 18:25:09 -0400

    search bar functionality

--- players.js
+++ players.js
@@ -31,8 +31,8 @@ function addTeam(el, index) {
 
 }
 
-$(document).ready(function() {
-
+function getDefaultScreen() {
+    
     $.getJSON("https://greenfieldleagues.com/data.json", function(data) {
         
         let teams = data.teams;
@@ -41,6 +41,77 @@ $(document).ready(function() {
 
         teams.forEach(addTeam); 
     
+    });
+
+}
+
+function addPlayer(el, index) {
+    $('.players .teams').append(`
+        <div class="player">
+            <img src="images/` + el.img + `">
+            <div class="info">
+                <p class="name"> #` + el.num + ` ` + el.name + `</p>
+                <p>` + el.club + `</p>
+                <p>` + el.hs + ` ('` + el.class + `')</p>
+                <p>Positions: ` + el.positions + `</p>
+            </div>
+        </div>
+    `);
+}
+
+function search_players() {
+
+    let search = $('.nav input').val().toLowerCase().replace(/\s+/g, '');
+
+    if(search == "") {
+        getDefaultScreen();
+    }
+
+    $('.players .teams').remove();
+    $('.players').append('<div class="teams"></div>')
+
+    $.getJSON("https://greenfieldleagues.com/data.json", function(data) {
+            
+        let teams = data.teams;
+
+        teams.forEach(function(el, index) {
+            let players = el.players;
+
+            players.forEach(function(el, index) {
+
+                let name = el.name.toLowerCase().replace(/\s+/g, '');
+
+                if(name.indexOf(search) >= 0) {
+                    addPlayer(el, index)
+                }
+
+            });
+
+        });
+
+        
+    });
+
+}
+
+$(document).ready(function() {
+
+    getDefaultScreen();    
+
+    $('.nav button.search').on('click',search_players);
+
+    $('.nav button.clear').on('click', function() {
+        $('.players .teams').remove();
+        $('.players').append('<div class="teams"></div>');
+        $('.nav input').val("");
+        getDefaultScreen()
+    });
+
+    $(document).on("keypress", "input", function(e){
+        if(e.which == 13){
+            var inputVal = $(this).val();
+            search_players()
+        }
     });
 
 });
